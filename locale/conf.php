d16fad1b4f571c130791b96b62836a984435dcef
Jan-Christoph Borchardt <JanCBorchardt@fsfe.org>
2011-05-22 20:54:44 +0000

    added German translation

--- locale/conf.php
+++ locale/conf.php
@@ -1,14 +1,13 @@
-<?php 
+<?php
 $locale = "en_US";
-if (isSet($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
 	$locale = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 5);
-  $locale = preg_replace("/(..)-(..)/e", "'\\1_'.strtoupper('\\2')", $locale);
+	$locale = preg_replace("/(..)-(..)/e", "'\\1_'.strtoupper('\\2')", $locale);
 }
-if (isSet($_GET["locale"])) $locale = $_GET["locale"];
+if(isset($_GET["locale"])) $locale = $_GET["locale"];
 
 putenv("LC_ALL=$locale");
 setlocale(LC_ALL, $locale);
 bindtextdomain("messages", "./locale");
 textdomain("messages");
-?> 
-
+?>
