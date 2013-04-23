bd2f073c1b5f833983d820e5f7dd0478086f872e
Iron Bishop <ironbishop@fsfe.org>
2013-04-23 18:28:38 +0200

    Never show no search results, see #15

--- js/default.js
+++ js/default.js
@@ -182,16 +182,12 @@ lp = $.extend(lp, {
 			var value = $search.val().toLowerCase();
 
 			// Hide or show projects depending if they match or not
-			$('#categories ul li .name').each(function(idx, project) {
-				var $project = $(project);
-				if (value && $project.text().toLowerCase().indexOf(value) == -1) {
-					$project.parents('li').hide();
-				} else {
-					$project.parents('li').show();
-				}
-			} );
-
-			lp.categoriesDisplay();
+			var find = $('#categories ul li').filter(function() {return this.id.toLowerCase().match(value);});
+			if (find.length) {
+			  $('#categories ul li').hide();
+			  find.show();
+			  lp.categoriesDisplay();
+			}
 //		} );
 	},
 
