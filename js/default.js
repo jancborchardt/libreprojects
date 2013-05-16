7d96d785c2b79a8bacc8f66858bdb356efcaba3d
Iron Bishop <ironbishop@fsfe.org>
2013-05-16 13:58:34 +0200

    Revert "show/hide elements in response to search"
    This reverts commit fb9d48cd707f47bc975ea2619a9d926454a9279d.

--- js/default.js
+++ js/default.js
@@ -207,9 +207,9 @@ lp = $.extend(lp, {
 			if ($category.find('li').filter(function() {
 				return $(this).css('display') != 'none';
 			} ).length) {
-				$category.slideDown().prev().show();
+				$category.show().prev().show();
 			} else {
-				$category.hide().prev().slideUp();
+				$category.hide().prev().hide();
 			}
 		} );
 	},
